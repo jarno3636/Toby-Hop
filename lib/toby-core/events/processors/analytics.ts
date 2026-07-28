c3abe55136241827294a3a93197c0401269a8ec6
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2026-07-27 19:54:04 -0500

    Add analytics event processor

---
+++ lib/toby-core/events/processors/analytics.ts
@@ -0,0 +1,11 @@
+import type {
+  EventProcessor,
+} from '../dispatcher';
+
+export const runAnalyticsProcessor: EventProcessor = {
+  name: 'Analytics',
+
+  async handle() {
+    // TODO
+  },
+};
