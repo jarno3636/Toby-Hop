272a10d1fbe02067538ffa180dcc07c5724c129c
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2026-07-27 19:53:28 -0500

    Add notification processor skeleton

---
+++ lib/toby-core/events/processors/notifications.ts
@@ -0,0 +1,11 @@
+import type {
+  EventProcessor,
+} from '../dispatcher';
+
+export const runNotificationProcessor: EventProcessor = {
+  name: 'Notifications',
+
+  async handle() {
+    // TODO
+  },
+};
